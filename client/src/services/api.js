dea2f36b387fb23e729feb6dce36b89fcb093c9a
thanhnhudn <thanhnhudn@gmail.com>
2025-01-13 21:29:05 +0700

    try to deploy on render.com

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 // })
 
 const instance = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://giveandtake-api.onrender.com/api',
   timeout: 5000
   // headers: {
   //     Authorization: `Bearer ${token}`
